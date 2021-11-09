0963cc43dc71f3638521ab49b505d81153ddd8de
Чертыковцева Оксана <certykovcevaoksana@MacBook-Air-Certykovceva.local>
2021-11-09 15:24:39 +0500

    123

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let min = 1;
+let max = 20;
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; 
+  }
